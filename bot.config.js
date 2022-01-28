32f8a945942121b75fca5f5022e002d64b14e018
Ximen55 <47807825+Ximen55@users.noreply.github.com>
2022-01-28 21:09:47 +0800

    Update bot.config.js

--- bot.config.js
+++ bot.config.js
@@ -1,9 +1,9 @@
 const linebot = require('linebot');
 
 var bot = linebot({
-    channelId: '請輸入',
-    channelSecret: '請輸入',
-    channelAccessToken: '請輸入',
+    channelId: '1656846087',
+    channelSecret: 'b559e418cfd7fbfb706bebebd9a12e46',
+    channelAccessToken: 'OJQVILi7jyCxHU+nPtuERPqgUpdIESvHy9Lb71y264x4o7/c8o5Xw7DzwhDhlLgCm+3rXAw1YZ5d6UoSgB6Ned3iux+ZX7KfokQJ4Rd0Ekq6cyMH6zYTwcUv/yEvX2CsaDtK8I2UblmHDzDyblL9wgdB04t89/1O/w1cDnyilFU=',
 })
 
 module.exports = bot
