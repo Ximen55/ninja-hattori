9575ce3e8711954582052325fea721a7a58156d8
Ximen55 <47807825+Ximen55@users.noreply.github.com>
2022-02-05 12:39:40 +0800

    Update bot.config.js

--- bot.config.js
+++ bot.config.js
@@ -1,9 +1,9 @@
 const linebot = require('linebot');
 
 var bot = linebot({
-    channelId: '1656846087',
-    channelSecret: 'b559e418cfd7fbfb706bebebd9a12e46',
-    channelAccessToken: 'OJQVILi7jyCxHU+nPtuERPqgUpdIESvHy9Lb71y264x4o7/c8o5Xw7DzwhDhlLgCm+3rXAw1YZ5d6UoSgB6Ned3iux+ZX7KfokQJ4Rd0Ekq6cyMH6zYTwcUv/yEvX2CsaDtK8I2UblmHDzDyblL9wgdB04t89/1O/w1cDnyilFU=',
+    channelId: '1656864897',
+    channelSecret: '5fac6fc9074dea25e2b6ad9aa9fc0da3',
+    channelAccessToken: 'URX1+fPkbtXXFxyLjIfeji2PwpJ9IV51mTtUnujtNOhuLd4/gWAI8hKfbgfw6zC3KFGU1+QRgLZftMNiXefMblca+UGZ4Mhdp3+qqkiWqx4GlBlozcont0VsQyYczH0tRSdhZEr3ZjweraR1wLAmrgdB04t89/1O/w1cDnyilFU=',
 })
 
 module.exports = bot
